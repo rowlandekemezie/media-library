a798a38a5476620404a8a8b08f9349fd1bab2dec
andela-rekemezie <rowland.ekemezie@andela.com>
2016-07-29 00:37:23 +0100

    Add css styles to presentation

--- src/containers/GalleryContainer.js
+++ src/containers/GalleryContainer.js
@@ -8,21 +8,37 @@ import * as Actions from '../actions/actionTypes';
 class GalleryContainer extends Component {
   constructor(props, context) {
     super(props, context);
-    this.state = {};
+    this.state = {
+      selectedImage: Object.assign({}, props.images)[0]
+    };
+    this.handleSelect = this.handleSelect.bind(this);
   };
 
+  handleSelect(selectedImage) {
+    console.log(selectedImage, 'This is the handle image selected')
+
+  }
+
   render() {
+    console.log(this.state.selectedImage);
     const {images} = this.props;
+    const {selectedImage} = this.state;
     return (
-      <div className="">
-        <div className="">
+      <div className="image-gallery">
+        <div className="gallery-image">
+          <div key={selectedImage.id}><img src={selectedImage.watchHref}/></div>
+        </div>
+        <div className="image-scroller">
           {images.map(image =>(
             <div key={image.id}>
-              <h1> {image.title} </h1>
-              <img src={image.watchHref}/>
+              <h6> {image.title} </h6>
+              <div key={image.id} onClick={this.handleSelect(image)}>
+                <img src={image.watchHref}/>
+              </div>
             </div>
           ))}
           <div>
+
           </div>
         </div>
       </div>
@@ -31,7 +47,7 @@ class GalleryContainer extends Component {
 }
 
 GalleryContainer.propTypes = {
-  images: PropTypes.array.isRequired
+  images: PropTypes.array
 };
 
 function mapStateToProps(state) {
