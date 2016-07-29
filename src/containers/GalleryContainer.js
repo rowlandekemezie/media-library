55dc5d000a32cd25e078f38b623498e6a558cd56
andela-rekemezie <rowland.ekemezie@andela.com>
2016-07-29 01:41:17 +0100

    Add Ajax call to flickr

--- src/containers/GalleryContainer.js
+++ src/containers/GalleryContainer.js
@@ -9,20 +9,37 @@ class GalleryContainer extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      selectedImage: Object.assign({}, props.images)[0]
+      images: [...props.images],
+      selectedImage: [...props.images][0]
     };
     this.handleSelect = this.handleSelect.bind(this);
   };
 
   handleSelect(selectedImage) {
-    console.log(selectedImage, 'This is the handle image selected')
+    // console.log(selectedImage, 'This is the handle image selected')
+    // this.setState({selectedImage})
+  }
 
+  componentDidMount() {
+    const API_KEY = 'a46a979f39c49975dbdd23b378e6d3d5';
+    const API_ENDPOINT = `https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=${API_KEY}&format=json&nojsoncallback=1&per_page=5`;
+
+  console.log(this.props.images, 'component did mount')
+    fetch(API_ENDPOINT).then((response) => {
+      return response.json().then((json) => {
+        const images = json.photos.photo.map(({farm, server, id, secret}) => {
+          return {
+            id: id,
+            watchHref: `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`
+          }
+        });
+        this.setState({images, selectedImage:images[0]})
+      })
+    })
   }
 
   render() {
-    console.log(this.state.selectedImage);
-    const {images} = this.props;
-    const {selectedImage} = this.state;
+    const {images, selectedImage} = this.state;
     return (
       <div className="image-gallery">
         <div className="gallery-image">
