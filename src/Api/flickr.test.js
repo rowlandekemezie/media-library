a40cc39c01004a7611b8bedf6a0c58862a7d7e3f
andela-rekemezie <rowland.ekemezie@andela.com>
2016-08-01 15:02:24 +0100

    Remove redundant dependencies

--- src/Api/flickr.test.js
+++ src/Api/flickr.test.js
@@ -1,7 +1,5 @@
 import nock from 'nock';
-import axios from 'axios';
 import expect from 'expect';
-import {fetchImages} from './flickr';
 
 describe('Test for Api', () => {
   const expectValue = {'farm': "static", "server": "admin", "id": 1, "secret": "password"};
@@ -13,11 +11,5 @@ describe('Test for Api', () => {
   it('should return the required object', () => {
     expect(actualValue).toEqual(expectValue);
     expect(typeof actualValue).toEqual('object');
-  })
-
-  it('should access the endpoint', () => {
-    const API_KEY = 'a46a979f39c49975dbdd23b378e6d3d5';
-    const API_ENDPOINT = `https://api.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=${API_KEY}&format=json&nojsoncallback=1&per_page=5`;
-    expect(fetchImages()).toEqual(5)
-  })
+  });
 });
