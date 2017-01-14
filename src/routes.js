9f61c6949b009668694299a11d743a38858d2ee3
mentrie <rowlandresource@gmail.com>
2017-01-15 00:48:58 +0100

    Fix missing link for library

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-// import MediaGalleryPage from './containers/MediaGalleryPage'
+import MediaGalleryPage from './containers/MediaGalleryPage';
 import App from './containers/App';
 import HomePage from './components/HomePage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
+    <Route path="library" component={MediaGalleryPage} />
   </Route>
 );
 
